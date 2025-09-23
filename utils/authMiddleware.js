f6800090cdc1c83d4beb1d45290db6d4accaf1e3
Thai.Nguyen <thai.nguyen@milientsoftware.com>
2025-09-24 06:09:26 +0700

    fix cookie

--- utils/authMiddleware.js
+++ utils/authMiddleware.js
@@ -5,26 +5,48 @@ const jwt = require('jsonwebtoken');
  */
 const extractToken = (req) => {
   let token = null;
-
+  console.log('Extracting token from request...');
   // Check Authorization header
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    console.log('Token found in Authorization header');
     token = req.headers.authorization.substring(7);
   }
   // Check query parameter
   else if (req.query && req.query.token) {
+    console.log('Token found in query parameter');
     token = req.query.token;
   }
   // Check cookies - try multiple possible cookie names
   else if (req.cookies) {
+    console.log('Checking cookies for token');
     // Try pluriell_token first (our app's cookie)
     if (req.cookies.pluriell_token) {
+      console.log('Token found in pluriell_token cookie');
       token = req.cookies.pluriell_token;
+    }
+    // Try pluriell-specific client token
+    else if (req.cookies.pluriell_token_client) {
+      console.log('Token found in pluriell_token_client cookie');
+      token = req.cookies.pluriell_token_client;
     }
     // Try access_token (from SSO Gateway)
     else if (req.cookies.access_token) {
+      console.log('Token found in access_token cookie');
       token = req.cookies.access_token;
     }
+    // Try shared SSO tokens
+    else if (req.cookies.sso_token_client) {
+      console.log('Token found in sso_token_client cookie');
+      token = req.cookies.sso_token_client;
+    }
+    else if (req.cookies.sso_token) {
+      console.log('Token found in sso_token cookie');
+      token = req.cookies.sso_token;
+    }
   }
+  console.log('Token after cookie check:', token);
+  // Check localStorage via client-side script (this won't work server-side)
+  // This is handled by auth-helper.js on the client side
 
   return token;
 };
@@ -35,6 +57,7 @@ const extractToken = (req) => {
 const verifyToken = (req, res, next) => {
   try {
     const token = extractToken(req);
+    console.log('Extracted Token:', token);
     
     if (!token) {
       return res.status(401).json({
